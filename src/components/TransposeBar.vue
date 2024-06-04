c2ae18061c53befd46db1d20c8e1a930e55ae135
z6dabrata <z6dabrata@gmail.com>
2024-06-04 22:41:22 +0000

    Move transpose bar to a separate component

---
+++ src/components/TransposeBar.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="bar">
+    <button @click="transposeUp">      
+      <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 4L4 12H8V20H16V12H20L12 4Z" fill="currentColor"/>
+      </svg>
+    </button>
+    <span class="key">{{ props.songKey ?? '?' }}</span> 
+    <button @click="transposeDown">
+      <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 20L20 12H16V4H8V12H4L12 20Z" fill="currentColor"/>
+      </svg>
+    </button>
+    |
+    <button v-for="k of simpleKeys" @click="() => transposeTo(k)" :disabled="props.songKey  === k">{{ k }}</button>
+    <button v-if="magicKey" @click="() => transposeByNTones(magicKey.shift)" :disabled="magicKey.shift === 0">{{
+      magicKey.chordName }}</button>
+  </div>
+</template>
+
+<script setup>
+import { defineEmits, defineProps, computed } from 'vue';
+import { analyzeSong} from "../chords/analysis.ts";
+import { transposeChord, isMajorKey, isMinorKey, calcKeyDifference } from "../chords/chords.ts";
+
+
+const props = defineProps({
+  songKey: String,
+  shift: {
+    type: Number,
+    required: true
+  },
+  transposedSong: Array  
+});
+
+
+const emit = defineEmits(['update:shift']);
+
+const simpleKeys = computed(() => {
+  if (!props.songKey) {
+    return [];
+  }
+
+  if (isMajorKey(props.songKey)) {
+    return ['C', 'G']
+  }
+
+  if (isMinorKey(props.songKey)) {
+    return ['Am', 'Em']
+  }
+});
+
+
+const transposeUp = () => {  
+  emit('update:shift', props.shift + 1);
+};
+
+const transposeTonesNTones = (n) => {
+  emit('update:shift', props.shift + n);
+}
+
+const transposeTo = (newKey) => {
+  emit('update:shift', calcKeyDifference(props.songKey, newKey));  
+};
+
+const transposeDown = () => {
+  emit('update:shift', props.shift - 1);
+};
+
+const magicKey = computed(() => {
+  const {bestKey} = analyzeSong(props.transposedSong);
+ 
+  return {
+    chordName: props.songKey ? transposeChord(props.songKey, bestKey.shift) : undefined,
+    ...bestKey
+  };
+});
+
+
+</script>
+
+<style scoped lang="scss">
+
+.bar {
+  padding: 8px;
+  border-radius: 8px;;
+
+  display: flex;
+  align-items: center;
+  background: rgb(225, 235, 255);
+  font-size: 13px;
+
+  button {
+    background: transparent;
+    padding: 8px;
+    cursor: pointer;
+    border: 0 solid;
+
+    &:hover {
+      background-color: aliceblue;
+    }
+  }
+
+  .key {
+    font-size: 13px;
+    padding: 8px 0px;
+    display: block;
+    border-radius: 4px;
+    
+    cursor: pointer;
+  }
+}
+
+</style>
--- src/components/VSong.vue
+++ src/components/VSong.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="wrapper">
-    <div class="bar">
-      <button @click="transposeUp">🔺</button>
-      <span class="key">{{ key ?? '?' }}</span>
-      <button @click="transposeDown">🔻</button>
-
-      <button v-for="k of simpleKeys" @click="() => transposeTo(k)" :disabled="key === k">{{ k }}</button>
-      <button v-if="magicKey"  @click="() => transposeTonesNTones(magicKey.shift)" :disabled="magicKey.shift === 0">{{ magicKey.chordName }}</button>
-    </div>
+    <TransposeBar :songKey="key" v-model:shift="shift" :transposedSong="transposedSong" />
     <div class="song">
       <div :class="['line', line.type, line.tag, line.sectionEnd ? 'section-end' : '']" v-for="line in transposedSong"
         :key="line">
@@ -35,147 +28,55 @@
 </template>
 
 <script setup>
-import { parseChords, transpose, transposeChord, isMinorKey, calcKeyDifference, isMajorKey } from "../chords/chords.ts";
+import { parseChords, transpose, transposeChord, calcKeyDifference, isMajorKey } from "../chords/chords.ts";
 import { computed, ref } from "vue";
 import Chord from "./Chord.vue";
 import { tagLines } from "../chords/tag_lines.ts";
-import { analyzeSong} from "../chords/analysis.ts";
-
+import TransposeBar from "./TransposeBar.vue";
 
 
 const { song } = defineProps(['song'])
 
-const transposeTones = ref(0);
+const shift = ref(0);
 
 const parsedSong = computed(() => {
   return parseChords({ text: song.body });
 });
 
+const transposedSong = computed(() => {
+  return transpose(tagLines(parsedSong.value), shift.value);
+});
+
 const key = computed(() => {
   if (song.data?.key) {
-    return transposeChord(song.data.key, transposeTones.value);
+    return transposeChord(song.data.key, shift.value);
   } else {
     return undefined;
   }
 });
 
-const simpleKeys = computed(() => {  
-  if (!key.value) {
-    return [];    
-  }
-
-  if (isMajorKey(key.value)) {
-    return ['C', 'G']
-  }
-
-  if (isMinorKey(key.value)) {
-    return ['Am', 'Em']
-  }
-});
-
-const taggedSong = computed(() => {
-  return tagLines(parsedSong.value);
-});
-
-const transposedSong = computed(() => {
-  return transpose(taggedSong.value, transposeTones.value);
-});
-
-
-const magicKey = computed(() => {
-  
-  
-  const {bestKey} = analyzeSong(transposedSong.value);
- 
-
-  return {
-    chordName: key.value ? transposeChord(key.value, bestKey.shift) : undefined,
-    ...bestKey
-  };
-});
-
-
-const transposeUp = () => {
-  transposeTones.value += 1;
-};
-
-
-const transposeTonesNTones = (n) => {
-  transposeTones.value += n;
-}
-
-const transposeTo = (newKey) => {
-  transposeTones.value += calcKeyDifference(key.value, newKey);
-};
-
-const transposeDown = () => {
-  transposeTones.value -= 1;
-};
 </script>
 
 <style scoped lang="scss">
 .wrapper {
   margin: auto;
-}
 
-
-.song {
-  font-family: monospace;
+  .song {
+    font-family: monospace;
+    display: table;
+    width: auto;
+  }
 }
 
 .line {
   padding: 4px 24px;
   display: flex;
-}
+  white-space: nowrap;
 
 
-
-
-.bar {
-  margin-bottom: 24px;
-  display: flex;
-  align-items: center;
-
-  button {
-    background: #eee;
-    border: 1px #dddddd solid;
-    padding: 8px;
-    border-radius: 4px;
-    margin-right: 4px;
-    cursor: pointer;
+  .lyrics {
+    white-space: pre;
   }
-
-  .key {
-    background: #fff;
-    padding: 8px 16px;
-    display: block;
-    border-radius: 4px;
-    margin-right: 4px;
-    cursor: pointer;
-  }
-}
-
-.song {
-  display: table;
-  width: auto;
-}
-
-.line {
-  display: table-row;
-
-  $tag-backgrounds: (
-    'verse': rgb(255, 248, 154),
-    'chorus': rgb(215, 255, 155),
-    'bridge': rgb(255, 232, 232),
-    'intro': rgb(240, 240, 240)
-  );
-
-  @each $tag, $color in $tag-backgrounds {
-    &.tag-#{$tag} {
-      background: $color;
-    }
-  }
-
 
   &.emptyLine {
     padding: 12px 0;
@@ -199,21 +100,21 @@ const transposeDown = () => {
       margin-left: -24px;
     }
   }
+
+
+  $tag-backgrounds: (
+    'verse': rgb(255, 248, 154),
+    'chorus': rgb(215, 255, 155),
+    'bridge': rgb(255, 232, 232),
+    'intro': rgb(240, 240, 240)
+  );
+
+@each $tag, $color in $tag-backgrounds {
+  &.tag-#{$tag} {
+    background: $color;
+  }
 }
-
-
-
-
-
-
-
-/* 
-.tag {
-  background:rgb(255, 248, 154);
-  width: 100px;
-  padding: 8px;
-  border-radius: 0  0 8px 8px;
-} */
+}
 
 
 
@@ -228,12 +129,5 @@ const transposeDown = () => {
   white-space: pre;
 }
 
-.line {
-  display: flex;
-  white-space: nowrap;
-}
 
-.lyrics {
-  white-space: pre;
-}
 </style>
